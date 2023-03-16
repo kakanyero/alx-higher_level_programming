70dacf29aef0f8e826b109bf0582d40a983278b2
kakanyero <kakanyero142@gmail.com>
2023-03-16 09:28:44 +0300

    task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,29 +1,25 @@
 #!/usr/bin/python3
-def to_subtract(list_num):
-    to_sub = 0
-    max_list = max(list_num)
-    for n in list_num:
-        if max_list > n:
-            to_sub += n
-            return (max_list - to_sub)
-        def roman_to_int(roman_string):
-            if not roman_string:
+def roman_to_int(roman_string: str):
+    if roman_string is None or type(roman_string) != str:
+        return 0
+    data = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    numbers = [data[x] for x in roman_string] + [0]
+    rep = 0
+    for i in range(len(numbers) - 1):
+        if numbers[i] >= numbers[i+1]:
+            rep += numbers[i]
+        else:
+            rep -= numbers[i]
+            return rep
+        Advance task
+        vi 100-weight_average.py
+        #!/usr/bin/python3
+        def weight_average(my_list=[]):
+            if not my_list:
                 return 0
-            if not isinstance(roman_string, str):
-                return 0
-            rom_n = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
-            list_keys = list(rom_n.keys())
             num = 0
-            last_rom = 0
-            list_num = [0]
-            for ch in roman_string:
-                for r_num in list_keys:
-                    if r_num == ch:
-                        if rom_n.get(ch) <= last_rom:
-                            num += to_subtract(list_num)
-                            list_num = [rom_n.get(ch)]
-                        else:
-                            list_num.append(rom_n.get(ch))
-                            last_rom = rom_n.get(ch)
-                            num += to_subtract(list_num)
-                            return (num)
+            den = 0
+            for tup in my_list:
+                num += tup[0] * tup[1]
+                den += tup[1]
+                return (num / den)
