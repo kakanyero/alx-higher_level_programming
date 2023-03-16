00836fecade4e182294faa7a47959095d9f2f662
kakanyero <kakanyero142@gmail.com>
2023-03-16 09:22:46 +0300

    task 8

--- 0x04-python-more_data_structures/8-simple_delete.py
+++ 0x04-python-more_data_structures/8-simple_delete.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-def simple_delete(a_dictionary, key=""):
-    if a_dictionary.get(key) is not None:
-        del a_dictionary[key]
-        return (a_dictionary)
+def simple_delete(my_dict, key=""):
+    my_dict.pop(key, None)
+    return my_dict
