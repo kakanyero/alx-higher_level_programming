1f1f4653c24c706e768244170da5ece0b96fd0af
kakanyero <kakanyero142@gmail.com>
2023-03-16 09:10:21 +0300

    task o

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
+# 0-square_matrix_simple.py
 def square_matrix_simple(matrix=[]):
-    squared = []
-    for line in matrix:
-        squared.append([c**2 for c in line])
-        return squared
+    """Compute the square value of all integers of a matrix."""
+            return ([list(map(lambda x: x * x, row)) for row in matrix])
