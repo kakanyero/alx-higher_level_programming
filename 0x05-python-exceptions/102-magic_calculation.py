1b7d02401d1a50fd9173f84d6672f43f2d5ff797
kakanyero <kakanyero142@gmail.com>
2023-03-27 17:30:54 +0300

    2 tasks changed

--- 0x05-python-exceptions/102-magic_calculation.py
+++ 0x05-python-exceptions/102-magic_calculation.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 def magic_calculation(a, b):
     result = 0
     for i in range(1, 3):
@@ -10,4 +11,4 @@ def magic_calculation(a, b):
         except:
             result = b + a
             break
-        return (result)
+    return (result)
