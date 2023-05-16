24e148bf3c17af36344e2e05fa45e16f7a3bb014
kakanyero <kakanyero142@gmail.com>
2023-05-16 18:10:55 +0300

    twist

--- 0x0D-SQL_introduction/0-list_databases.sql
+++ 0x0D-SQL_introduction/0-list_databases.sql
@@ -1,2 +1,4 @@
--- lists all databases of your MySQL server.
-SHOW DATABASES;
+-- lists all the tables of a database in your MySQL server.
+-- The database name will be passed as argument of mysql command
+
+SHOW TABLES;
