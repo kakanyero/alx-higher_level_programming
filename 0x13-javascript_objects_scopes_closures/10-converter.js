d4ccefe5b126035df0ab03c2b70a34f37d455162
kakanyero <kakanyero142@gmail.com>
2023-06-13 14:32:42 +0300

    all

---
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -0,0 +1,73 @@
+#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};#!/usr/bin/node
+
+exports.converter = function (base) {
+  function myConverter (n) {
+    return n.toString(base);
+  }
+
+  return myConverter;
+};
